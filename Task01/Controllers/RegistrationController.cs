acafd2ed79069c0833b335e77fc9dc7a4926755c
Valera <vlaus0@gmail.com>
2023-07-07 15:04:58 +0300

    Добавьте файлы проекта.

---
+++ Task01/Controllers/RegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task01.Models;
+
+namespace Task01.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegistrationFormModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                if (form.Product == "Основи" && form.DesiredConsultationDate.DayOfWeek == DayOfWeek.Monday)
+                {
+                    ModelState.AddModelError("DesiredConsultationDate", "Консультація щодо продукту «Основи» не може проходити по понеділках");
+                    return View(form);
+                }
+
+                return RedirectToAction("Success");
+            }
+
+            return View(form);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task01/Models/RegistrationFormModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Task01.Controllers;
+
+namespace Task01.Models
+{
+    public class RegistrationFormModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [FutureDate(ErrorMessage = "Дата консультації має бути у майбутньому")]
+        [NonWeekend(ErrorMessage = "Дата консультації не повинна потрапляти на вихідні дні")]
+        public DateTime DesiredConsultationDate { get; set; }
+        [Required]
+        public string Product { get; set; }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                return date > DateTime.Now;
+            }
+
+            return false;
+        }
+    }
+
+    public class NonWeekendAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+            }
+
+            return false;
+        }
+    }
+}
